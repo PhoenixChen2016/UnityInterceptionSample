b6b340284d625a31825536bd5b0f5fa5955262d6
Phoenix <fphoenix923@hotmail.com>
2020-11-30 10:22:22 +0800

    加入VirtualMethod的範例

---
+++ Library/NotifyService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Library
+{
+	public class NotifyService
+	{
+		public virtual void Send()
+		{
+			Console.WriteLine("Virtual Send");
+		}
+	}
+}
--- UnityInterceptionSample/Program.cs
+++ UnityInterceptionSample/Program.cs
@@ -2,6 +2,7 @@
 using Library;
 using Unity;
 using Unity.Interception;
+using Unity.Interception.Interceptors;
 using Unity.Interception.Interceptors.InstanceInterceptors.InterfaceInterception;
 using Unity.Interception.Interceptors.TypeInterceptors.VirtualMethodInterception;
 
@@ -12,12 +13,24 @@ namespace UnityInterceptionSample
 		static void Main(string[] args)
 		{
 			// 不使用container的應用
-			var serviceWithoutDI = Intercept.ThroughProxy<IUserService>(
-				new UserService(),
-				new InterfaceInterceptor(),
-				new[] { new LogBehavior() });
+			var interfaceInterceptor = new InterfaceInterceptor();
+
+			var warppedInstance = interfaceInterceptor.CreateProxy(typeof(IUserService), new UserService());
+			warppedInstance.AddInterceptionBehavior(new LogBehavior());
+
+			var serviceWithoutDI = (IUserService)warppedInstance;
 
 			serviceWithoutDI.AddUser("tester", "aa@bb.cc");
+
+			var virtualMethodInterceptor = new VirtualMethodInterceptor();
+
+			var warppedType = virtualMethodInterceptor.CreateProxyType(typeof(NotifyService));
+
+			var warpper = (IInterceptingProxy)Activator.CreateInstance(warppedType)!;
+
+			warpper.AddInterceptionBehavior(new LogBehavior());
+
+			((NotifyService)warpper).Send();
 
 			Console.ReadKey();
 		}
